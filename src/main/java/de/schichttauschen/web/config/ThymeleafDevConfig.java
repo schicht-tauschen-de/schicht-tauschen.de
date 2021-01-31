6c7da547cef6c08f1c6757b04e9fd814e3a7f386
Jan Brodda <git@janbrodda.de>
2021-01-31 15:13:41 +0100

    enable instant live reload for thymeleaf templates on dev profile

---
+++ src/main/java/de/schichttauschen/web/config/ThymeleafDevConfig.java
@@ -0,0 +1,31 @@
+package de.schichttauschen.web.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Profile("dev")
+public class ThymeleafDevConfig {
+    private final ThymeleafProperties properties;
+
+    @Value("${spring.thymeleaf.templates_root:}")
+    private String templatesRoot;
+
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(properties.getSuffix());
+        resolver.setPrefix(templatesRoot);
+        resolver.setTemplateMode(properties.getMode());
+        resolver.setCacheable(properties.isCache());
+        return resolver;
+    }
+}
