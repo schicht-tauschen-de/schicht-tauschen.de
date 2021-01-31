b98070e680c7e3ef4651aab7ee114035414a8b84
Jan Brodda <git@janbrodda.de>
2021-01-31 12:27:42 +0100

    implement filter to redirect to primary app domain

---
+++ src/main/java/de/schichttauschen/web/filter/DomainRedirectFilter.java
@@ -0,0 +1,32 @@
+package de.schichttauschen.web.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class DomainRedirectFilter implements Filter {
+    @Value("${app.domain}")
+    private String appDomain;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        if (!httpRequest.getServerName().equalsIgnoreCase(appDomain)) {
+            httpResponse.sendRedirect(
+                    "https://"
+                            + appDomain
+                            + httpRequest.getRequestURI()
+                            + (StringUtils.isNotEmpty(httpRequest.getQueryString()) ? "?" + httpRequest.getQueryString() : Strings.EMPTY))
+            ;
+        } else filterChain.doFilter(httpRequest, httpResponse);
+    }
+}
