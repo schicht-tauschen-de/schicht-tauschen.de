77817a7c3fe4a09d14dd5c887e736775257cfa48
Jan Brodda <git@janbrodda.de>
2021-02-01 21:53:28 +0100

    fix non-running test, switch to TestNG annotation

--- src/test/java/de/schichttauschen/web/WebApplicationTests.java
+++ src/test/java/de/schichttauschen/web/WebApplicationTests.java
@@ -1,7 +1,7 @@
 package de.schichttauschen.web;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class WebApplicationTests {
