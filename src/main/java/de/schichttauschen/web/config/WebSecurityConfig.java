83a4aa6a527c01eb2748b92f3c03c5e9613699df
Jan Brodda <git@janbrodda.de>
2021-02-03 22:19:09 +0100

    cleanup and simplify WebSecurityConfig

--- src/main/java/de/schichttauschen/web/config/WebSecurityConfig.java
+++ src/main/java/de/schichttauschen/web/config/WebSecurityConfig.java
@@ -1,44 +1,28 @@
 package de.schichttauschen.web.config;
 
-import de.schichttauschen.web.data.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AccountRepository accountRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/css/*", "/js/*", "/img/*").permitAll();
+        http
+                .authorizeRequests().antMatchers("/", "/api/public/**").permitAll()
 
-        http.authorizeRequests()
-                .antMatchers("/", "/api/public/**").permitAll();
+                .and().authorizeRequests().antMatchers("/css/*", "/js/*", "/img/*").permitAll()
 
-        http.cors().and().csrf().ignoringAntMatchers("/api/**");
+                .and().authorizeRequests().antMatchers("/h2-console", "/h2-console/*").permitAll()
 
-        http.authorizeRequests()
-                .antMatchers("/h2-console", "/h2-console/*").permitAll()
-                .and().csrf().ignoringAntMatchers("/h2-console/**")
-                .and().headers().frameOptions().sameOrigin();
+                .and().csrf().disable()
 
-        http.formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", RequestMethod.GET.name()))
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .permitAll();
+                .headers().frameOptions().sameOrigin();
     }
 }
