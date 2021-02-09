d582bae407ce8f7609e2b2ed366668e1148eadd4
Jan Brodda <git@janbrodda.de>
2021-02-09 13:04:55 +0100

    enable max. caching for static resources

--- src/main/java/de/schichttauschen/web/config/MvcConfig.java
+++ src/main/java/de/schichttauschen/web/config/MvcConfig.java
@@ -2,14 +2,17 @@ package de.schichttauschen.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -22,6 +25,12 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        interceptor.addCacheMapping(CacheControl.maxAge(365, TimeUnit.DAYS)
+                .noTransform()
+                .mustRevalidate(), "/img/*", "/css/*", "/js/*");
+        registry.addInterceptor(interceptor);
     }
 
     @Bean
