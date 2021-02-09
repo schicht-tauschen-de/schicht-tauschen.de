ddcd1a9046f5bfbf57a55ff44b370c8fc9414730
marco <kontakt@schmidt-webmedia.de>
2021-02-09 17:18:44 +0100

    Optimize Login and Registration form and use MessageKeys for formValidation

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,101 +1,145 @@
+let translatedMessages = null;
+
+function updateTranslatedMessages() {
+    $.ajax({
+        url: '/api/public/messages',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            translatedMessages = data;
+        }
+    });
+}
+
+$(document).ready(updateTranslatedMessages());
+
+function getTranslatedMessage(key) {
+    if(typeof translatedMessages[key] === 'undefined') {
+        return 'Missing Key [key]';
+    } else {
+        return translatedMessages[key];
+    }
+}
+
 let vueStartPage = null;
 const accountEndpoint = '/api/public/account';
-$(document).ready(function() {
-    vueStartPage = new Vue({
-        el: '#vueStartPage',
-        data: {
-            context: 'login',
+vueStartPage = new Vue({
+    el: '#vueStartPage',
+    data: {
+        context: 'login',
+        fullName: null,
+        userName: null,
+        email: null,
+        password: null,
+        message: null,
+        isError: true,
+        formValidationErrors: {
             fullName: null,
             userName: null,
             email: null,
-            password: null,
-            message: null,
-            isError: true,
-            formValidationErrors: []
+            password: null
+        }
+    },
+    computed: {
+        isRegistration: function() {
+            return this.context === 'registration';
         },
-        computed: {
-            isRegistration: function() {
-                return this.context === 'registration';
-            },
-            getMessageClass: function() {
-                return this.isError ? 'alert-danger' : 'alert-success';
+        getMessageClass: function() {
+            return this.isError ? 'alert-danger' : 'alert-success';
+        }
+    },
+    methods: {
+        changeContext: function(newContext) {
+            this.context = newContext;
+        },
+        onSubmit: function() {
+            if(this.context === 'registration') {
+                this.doRegistration();
+            }  else {
+                this.doLogin();
             }
         },
-        methods: {
-            changeContext: function(newContext) {
-                this.context = newContext;
-            },
-            doLogin: function () {
-                let that = this;
+        doLogin: function () {
+            let that = this;
+            $.ajax({
+                url: accountEndpoint + '/login',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    login: that.userName,
+                    password: that.password
+                },
+                success: function (data) {
+                    that.isError = !data.authenticated;
+                    that.message = data.message;
+                },
+                error: function() {
+                    that.isError = true;
+                    that.message = 'Beim Login ist ein Fehler aufgetreten';
+                }
+            });
+        },
+        doRegistration: function () {
+            let that = this;
+            if(that.validateRegistrationForm()) {
                 $.ajax({
-                    url: accountEndpoint + '/login',
+                    url: accountEndpoint + '/register',
                     type: 'POST',
                     dataType: 'json',
-                    data: {
+                    contentType: 'application/json',
+                    data: JSON.stringify({
                         login: that.userName,
+                        name: that.fullName,
+                        email: that.email,
                         password: that.password
-                    },
-                    success: function (data) {
-                        that.isError = !data.authenticated;
-                        if(data.authenticated) {
-                            let name = data.account.name;
-                            that.message = 'Hallo ' + name + ',<br>der Login war erfolgreich';
+                    }),
+                    success: function (isSuccessful) {
+                        that.isError = !isSuccessful;
+                        that.message = data.message;
+                        if(isSuccessful) {
+                            that.message = 'Registrierung erfolgreich. Bitte gucke in dein E-Mail Postfach nach.';
                         } else {
-                            that.message = 'Leider war der Login nicht erfolgreich';
+                            that.message = 'Registrierung nicht erfolgreich!';
                         }
                     },
                     error: function() {
                         that.isError = true;
-                        that.message = 'Beim Login ist ein Fehler aufgetreten';
+                        that.message = 'Bei der Registrierung ist ein Fehler aufgetreten';
                     }
                 });
-            },
-            doRegistration: function () {
-                let that = this;
-                if(that.validateRegistrationForm()) {
-                    $.ajax({
-                        url: accountEndpoint + '/register',
-                        type: 'POST',
-                        dataType: 'json',
-                        contentType: 'application/json',
-                        data: JSON.stringify({
-                            login: that.userName,
-                            name: that.fullName,
-                            email: that.email,
-                            password: that.password
-                        }),
-                        success: function (isSuccessful) {
-                            that.isError = !isSuccessful;
-                            if(isSuccessful) {
-                                that.message = 'Registrierung erfolgreich. Bitte gucke in dein E-Mail Postfach nach.';
-                            } else {
-                                that.message = 'Registrierung nicht erfolgreich!';
-                            }
-                        },
-                        error: function() {
-                            that.isError = true;
-                            that.message = 'Bei der Registrierung ist ein Fehler aufgetreten';
-                        }
-                    });
-                }
-            },
-            validateRegistrationForm: function () {
-                this.formValidationErrors = [];
-                if(this.userName === null)
-                    this.formValidationErrors.push('Du musst einen Benutzernamen wählen');
-
-                if(this.fullName === null)
-                    this.formValidationErrors.push('Du musst deinen Namen angeben');
-
-                if(this.email === null)
-                    this.formValidationErrors.push('Du musst deine E-Mail Adresse angeben');
-
-                if(this.password === null)
-                    this.formValidationErrors.push('Du musst ein Passwort wählen');
-
-                return this.formValidationErrors.length === 0;
             }
-        }
-    });
+        },
+        validateRegistrationForm: function () {
+            let isValid = true;
+            this.formValidationErrors = {
+                fullName: null,
+                userName: null,
+                email: null,
+                password: null
+            };
 
+            if(this.userName === null) {
+                this.formValidationErrors.userName = getTranslatedMessage('formValidation.error.userName');
+                isValid = false;
+            }
+
+            if(this.fullName === null) {
+                this.formValidationErrors.fullName = getTranslatedMessage('formValidation.error.fullName');
+                isValid = false;
+            }
+
+            if(this.email === null) {
+                this.formValidationErrors.email = getTranslatedMessage('formValidation.error.email');
+                isValid = false;
+            }
+
+            if(this.password === null) {
+                this.formValidationErrors.password = getTranslatedMessage('formValidation.error.password');
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
 });
+
